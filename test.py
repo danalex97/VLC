fd36914d269abdb62c55c89dd7b9f0bdb1b564a7
Alexandru Dan <dan.alex97@yahoo.com>
2018-11-16 10:23:06 +0000

    Started chat app..

--- test.py
+++ test.py
@@ -2,50 +2,86 @@ import serial
 import time
 import sys
 
+import threading
+from threading import Lock
+
+
 class Device():
-    def __init__(self):
-        self.s = serial.Serial('/dev/ttyACM0',115200, timeout = 1)
+    def __init__(self, address):
+        self.serial = serial.Serial('/dev/ttyACM0', 115200, timeout = 1)
         time.sleep(2)
 
-    def send(self, message):
-        self.s.write(str.encode(message + "\n"))
+        # for listening
+        self.buffer = []
+        self.lock = Lock()
+        self.listener = threading.Thread(target=self.listen)
+        self.listener.start()
+
+        # initialize
+        self._send("a[{}]".format(address))
+        self._read()
+
+    def stop(self):
+        self.listener.join()
 
     def listen(self):
-        bts = self.s.read(10)
-        print(str(bts))
+        def _read(timeout=0):
+            """ reads from serial port until \n """
+
+            message = ""
+            byte = ""
+            while byte != "\n":
+                byte = self.serial.read(1)
+                byte = byte.decode('UTF-8')
+
+                if byte != "\n":
+                    message += byte
+            return message
+
+        while True:
+            message = _read()
+
+            self.lock.acquire()
+            self.buffer.append(message)
+            self.lock.release()
+
+    def _send(self, message):
+        """ send message to serial port """
+
+        self.serial.write(str.encode(message + "\n"))
+        time.sleep(0.5)
+
+    def _read(self):
+        message = None
+
+        self.lock.acquire()
+        if len(self.buffer) > 0:
+            message = self.buffer[0]
+            self.buffer = self.buffer[1:]
+        self.lock.release()
+
+        return message
+
+    def set_retransmissions(self, nbr):
+        self._send("c[1,0,{}]".format(nbr))
+        print(self._read())
+
+    def set_FEC(self, size):
+        self._send("c[0,1,{}]".format(size))
+        print(self._read())
+
+    def send_message(self, message, dest):
+        self._send("m[{}\0,{}]".format(message, dest))
+        print(self._read())
+
+    def broadcast(self, message):
+        self.send_message(message, "FF")
 
 if __name__ == "__main__":
-    d = Device()
-    s = d.s
-    # d.send("p")
-    # d.read()
+    d = Device("AB")
 
-    # s.write(b"r\n")
-    # time.sleep(0.1)
+    d.set_retransmissions(5)
+    d.set_FEC(30)
 
-    # #write to the device’s serial port
-    s.write(b"a[AB]\n") #set the device address to AB
-    time.sleep(0.1) #wait for settings to be applied
-    s.write(b"c[1,0,5]\n") #set number of retransmissions to 5
-    time.sleep(0.1) #wait for settings to be applied
-    s.write(b"c[0,1,30]\n") #set FEC threshold to 30 (apply FEC to packets with payload >= 30)
-    time.sleep(0.1) #wait for settings to be applied
-    s.write(b"m[hello world!\0,CD]\n") #send message to device with address CD
-    #
-    # #read from the device’s serial port (should be done in a separate thread)
-    message = ""
-    while True: #while not terminated
-     try:
-       byte = s.read(1) #read one byte (blocks until data available or timeout reached)
-       byte = byte.decode('UTF-8')
-
-       if byte=='\n': #if termination character reached
-         print(message) #print message
-         message = "" #reset message
-       else:
-         # print(byte)
-         message = message + byte #concatenate the message
-     except serial.SerialException:
-       continue #on timeout try to read again
-     except KeyboardInterrupt:
-       sys.exit() #on ctrl-c terminate program
+    d.send_message("hello world", "CD")
+    d.stop()
